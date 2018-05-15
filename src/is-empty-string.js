143ec96db62be5e038ef9b3a89217fdc786dce7b
Aitor <asakon28@gmail.com>
2018-05-15 21:43:20 +0200

    Update verification

--- src/is-empty-string.js
+++ src/is-empty-string.js
@@ -1,11 +1,11 @@
 import isString from "@rojo2/is-string";
 
 /**
- * Returns if the value passed is a string
+ * Returns if the value passed is a string and if it's empty
  *
  * @param {*} value - Value
- * @param {Object} options - Options 
- * @returns {string} - Returns if it is a string
+ * @param {Object} [options={}] - Options 
+ * @returns {string} - Returns if it is a string and if it's empty
  */
 export default function isEmptyString(value, options = {}) {
   const defaultOptions = {
@@ -20,8 +20,9 @@ export default function isEmptyString(value, options = {}) {
   if (!isString(value)) {
     return false;
   }
+
   if (opts.trim) {
-    return !value.trim();
+    return value.trim() === "";
   }
-  return !value.toString();
+  return value.toString() === "";
 }
